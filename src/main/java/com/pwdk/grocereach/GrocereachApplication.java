e0428ec64bed7b33f6a364d8e992f92b50efc0be
Vincentius <vincentiussss@gmail.com>
2025-07-17 14:19:54 +0700

    feat: initial commit

---
+++ src/main/java/com/pwdk/grocereach/GrocereachApplication.java
@@ -0,0 +1,13 @@
+package com.pwdk.grocereach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocereachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocereachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pwdk/grocereach/GrocereachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwdk.grocereach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocereachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
