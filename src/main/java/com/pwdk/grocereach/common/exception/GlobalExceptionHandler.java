ce93efd393e35da0d4ab1e4c470bb4fa8c739a16
adi wicaksono <muhammadiwicaksono@gmail.com>
2025-08-05 13:14:12 +0700

    feat: create custome exception for product endpoint

---
+++ src/main/java/com/pwdk/grocereach/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.pwdk.grocereach.common.exception;
+
+public class GlobalExceptionHandler extends RuntimeException {
+  public GlobalExceptionHandler(String message) {
+    super(message);
+  }
+}
